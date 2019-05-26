9110f68ebef667b0fe7f7ef666eb4b408a1e040c
Abdullah Alabasi <Tech3790@gmail.com>
2019-05-26 18:21:28 -0500

    first commit

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,7 +6,7 @@ import { settings } from '../../settings';
 // styles
 import * as styles from './Header.sass';
 
-export const Header: React.SFC = () => (
+export const Header: React.FC = () => (
   <header className={styles.header}>
     <h1>{settings.title}</h1>
   </header>
